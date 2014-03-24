f840af2cf5f9d9c5cf8dca0884cccb507d4b88b1
Aluno de Programação <prog@HOST>
2014-03-24 16:39:48 +0000

    Add verision id to MethodNotFoundException.

--- src/ist/meic/pa/commands/exception/MethodNotFoundException.java
+++ src/ist/meic/pa/commands/exception/MethodNotFoundException.java
@@ -1,6 +1,9 @@
 package ist.meic.pa.commands.exception;
 
 public class MethodNotFoundException extends CommandException {
+
+	private static final long serialVersionUID = -619815553971830606L;
+
 	public MethodNotFoundException(String className, String methodName, int numberOfArgs) {
 		super("The class " + className + " does not have a method named "
 				+ methodName + " with " + numberOfArgs + " arguments.");
