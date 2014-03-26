278e23cc82b96d0278af7f8f99a2caf20dd64900
mbote <mbotepm@gmail.com>
2014-03-26 21:19:51 +0000

    Finished1 cmd_i

--- src/ist/meic/pa/commands/Cmd_i.java
+++ src/ist/meic/pa/commands/Cmd_i.java
@@ -1,11 +1,17 @@
 package ist.meic.pa.commands;
 
 import java.util.List;
+
 import ist.meic.pa.Inspector;
+
 import java.lang.reflect.Field;
+
 import ist.meic.pa.commands.exception.CommandException;
 import ist.meic.pa.commands.exception.FieldNotFoundException;
 import ist.meic.pa.commands.exception.InvalidArgumentsException;
+import ist.meic.pa.commands.util.Parser;
+import ist.meic.pa.commands.util.ParserFactory;
+import ist.meic.pa.commands.util.exception.ParserException;
 
 
 public class Cmd_i implements Command {
@@ -18,6 +24,24 @@ public class Cmd_i implements Command {
 		return !(args.size() == 1);
 	}
 
+	private void findSetField(Class<?> clazz, Object obj,Inspector insp, String fieldName) throws IllegalArgumentException,
+			 IllegalAccessException, ParserException, NoSuchFieldException, CommandException{
+		try {
+			Field field = clazz.getDeclaredField(fieldName);  
+			field.setAccessible(true);
+			insp.modifyCurrentObj(field.get(obj));
+			field.setAccessible(false);			
+			insp.printCurrentObj();			
+			
+		} catch (NoSuchFieldException e) {
+			if (clazz != Object.class)
+				this.findSetField(clazz.getSuperclass(), obj,insp,fieldName);
+			else
+				throw new InvalidArgumentsException("i <name_of_the_field>");
+		}
+	}
+
+	
 	@Override
 	public void execute(Inspector insp, List<String> args) throws CommandException {
 		if (this.checkArgs(args)) {
@@ -25,22 +49,9 @@ public class Cmd_i implements Command {
 		}
 		
 		String fieldName = args.get(0);
-		try {
-			// pega o objecto corrente
-			Object obj = insp.obtainCurrentObj();
-
-			// pega a classe do objecto
-			Class<?> clazz = obj.getClass();
-			
-			// pega o atributo name do obj
-			Field field = clazz.getDeclaredField(fieldName);
-
-			// adiciona o obj como recente
-			field.setAccessible(true);
-			insp.modifyCurrentObj(field.get(obj));
-			field.setAccessible(false);
-			
-			insp.printCurrentObj();
+		try {			
+			Object obj = insp.obtainCurrentObj();			
+			this.findSetField(obj.getClass(),obj,insp,fieldName);
 			
 		} catch (NoSuchFieldException e) {
 			throw new FieldNotFoundException(fieldName);
@@ -49,6 +60,8 @@ public class Cmd_i implements Command {
 		} catch (IllegalArgumentException e) {
 			throw new RuntimeException(e);
 		} catch (IllegalAccessException e) {
+			throw new RuntimeException(e);
+		} catch (ParserException e) {			
 			throw new RuntimeException(e);
 		}
 	}
